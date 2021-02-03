da4e727f978cffb97f5e35fe26fb89be701a8fd7
schimin <robertojoaojr@gmail.com>
2021-02-03 10:02:52 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+import socket
+import os
+
+
+# HABILITA MOSTRAR MAIS DE UMA LINHA DE ERRO DE RESPOSTA DA API
+BUNDLE_ERRORS = True
+
+""" CONFIGURAÇÕES DE ENVIROMENT """
+# 'development' ou 'production', 'test'
+ENVIRONMENT = os.getenv('FLASK_ENV')
+if not ENVIRONMENT:
+    ENVIRONMENT = 'development'
+
+if ENVIRONMENT == 'development':
+    TESTING = True
+    DEBUG = True
+    ENV = 'development'
+    SERVER_NAME = 'localhost:6060'
+    print('Ambiente de Desenvolvimento', SERVER_NAME)
+
+if ENVIRONMENT == 'production':
+    TESTING = False
+    DEBUG = False
+    ENV = 'production'
+    #SERVER_NAME = 'api.grupoforus.com.br'
+    SERVER_NAME = 'ip:porta'
+    print('Ambiente de Produção', SERVER_NAME)
+
+if ENVIRONMENT == 'testing':
+    TESTING = True
+    DEBUG = False
+    ENV = 'testing'
+    #SERVER_NAME = 'api-test.grupoforus.com.br'
+    SERVER_NAME = 'ip:porta'
+    print('Ambiente de Teste', SERVER_NAME)
+
+
+print('ENV', ENV)
